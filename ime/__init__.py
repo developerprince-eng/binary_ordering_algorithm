40943b4ff193090cce32891c9575b09d17f87e3d
Prince Maposa <princekudzaimaposa94@gmail.com>
2019-04-25 17:13:11 +0200

    config File Edit

--- ime/__init__.py
+++ ime/__init__.py
@@ -5,12 +5,17 @@ from flask_migrate import Migrate
 
 
 lm = LoginManager()
-
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
-app.config['SECRET_KEY'] = "ime2019"
-# app.config.from_pyfile('config.py')
+# app.config['SECRET_KEY'] = "ime2019"
+app.config.update(
+    SECRETE_KEY = 'ime2019',
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+)
+app.config.from_pyfile('../config.py')
+
 db = SQLAlchemy(app)
+
 lm.init_app(app)
 migrate = Migrate(app, db)
 
