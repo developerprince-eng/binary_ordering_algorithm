f9b2de93ef5c28631e99ab7f6209440f7a49da92
Prince Maposa <princekudzaimaposa94@gmail.com>
2019-04-26 15:48:37 +0200

    progress

---
+++ ime/helpers/boa.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+class BOA:
+    def __init__(self):
+        self.is_obj = True
+        
+    def order(self, matrix):
+        row = matrix.shape[0]
+        col = matrix.shape[1]
+        lst = []
+
+        for i in matrix[1:]:
+            sum1 = 0
+            k = 3
+            for item in i[1:-1]:
+                item = int(item)
+                sum1 = int(sum1 + item * (2 ** (col - k)))
+                k = k + 1
+                i[-1] = sum1
+                lst.append(int(sum1))
+
+        lst1 = sorted(lst,reverse=True)
+
+        if lst == lst1:
+            pass
+        else:
+            matrix = matrix[np.argsort(matrix[:, -1].astype(int))[::-1]]
+
+        lst = []
+        for i in matrix[1:]:
+            i[-1] = 0
+        matrix = matrix.T
+
+
+        for i in matrix[1:-1]:
+            sum1 = 0
+            k=2
+            for item in i[1:]:
+
+                item = int(item)
+                sum1 = int(sum1 + item * (2 ** (row - k)))
+                k = k + 1
+                i[-1] = sum1
+                lst.append(int(sum1))
+        lst1 = sorted(lst,reverse=True)
+
+        if lst == lst1:
+            pass
+        else:
+            matrix = matrix[np.argsort(matrix[:, -1].astype(int))[::-1]]
+        for i in matrix[1:]:
+            i[-1]=0
+        matrix = matrix.T
+        return matrix
--- ime/views.py
+++ ime/views.py
@@ -6,6 +6,9 @@ from werkzeug.security import generate_password_hash
 from werkzeug.utils import secure_filename
 from . import app, lm
 from .models import User
+# import helpers.boa as BOA
+
+# boa = BOA.BOA()
 
 @app.errorhandler(403)
 def forbidden(e):
@@ -28,63 +31,14 @@ def server_error(e):
 
 @app.route("/")
 def index():
-    def order(a):
-
-        row = a.shape[0]
-        col = a.shape[1]
-        lst = []
-
-        for i in a[1:]:
-            sum1 = 0
-            k = 3
-            for item in i[1:-1]:
-                item = int(item)
-                sum1 = int(sum1 + item * (2 ** (col - k)))
-                k = k + 1
-                i[-1] = sum1
-                lst.append(int(sum1))
-
-        lst1 = sorted(lst,reverse=True)
-
-        if lst == lst1:
-            pass
-        else:
-            a = a[np.argsort(a[:, -1].astype(int))[::-1]]
-
-        lst = []
-        for i in a[1:]:
-            i[-1] = 0
-        a = a.T
-
-
-        for i in a[1:-1]:
-            sum1 = 0
-            k=2
-            for item in i[1:]:
-
-                item = int(item)
-                sum1 = int(sum1 + item * (2 ** (row - k)))
-                k = k + 1
-                i[-1] = sum1
-                lst.append(int(sum1))
-        lst1 = sorted(lst,reverse=True)
-
-        if lst == lst1:
-            pass
-        else:
-            a = a[np.argsort(a[:, -1].astype(int))[::-1]]
-        for i in a[1:]:
-            i[-1]=0
-        a = a.T
-        return a
-    matrix = np.array([("Ma","p1","p2","p3","p4","p5","p6","p7","1000"),("A",1,1,0,0,1,0,0,0),("B",0,0,1,0,0,0,0,1),("C",0,1,1,0,0,1,1,0),("D",0,0,0,1,0,0,0,1),("E",0,0,1,1,0,1,1,0),("F",1,1,0,0,1,0,0,0),(1000,0,0,0,0,0,0,0,100)])
-
-    while True:
-        buffer = order(matrix)
-        if (buffer==matrix).all()==True:
-            break
-        else:
-            matrix=buffer
-    print(buffer)
-
     return render_template("index.html")
+
+@app.route("/boa/input", methods=['GET', 'POST'])
+def boaInput():
+    if "step" not in request.form:
+        return render_template("boa-input.html", step="step-1")
+    elif request.form["step"] == "step-2":
+        return render_template("boa-input.html", step="step-2")
+    elif request.form["step"] == "step-3":
+        return render_template("boa-input.html", step="step-3")
+
