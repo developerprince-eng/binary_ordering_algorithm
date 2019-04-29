f0ff07dc3fca367b1fa1a7613790294f69d3abcd
Prince Maposa <princekudzaimaposa94@gmail.com>
2019-04-29 08:29:47 +0200

    changes

--- ime/views.py
+++ ime/views.py
@@ -6,39 +6,70 @@ from werkzeug.security import generate_password_hash
 from werkzeug.utils import secure_filename
 from . import app, lm
 from .models import User
-# import helpers.boa as BOA
+import ime.helpers.boa as BOA
 
-# boa = BOA.BOA()
+boa = BOA.BOA()
 
+#BAD REQUEST
+@app.errorhandler(400)
+def resource_error(e):
+    return render_template('400.html'), 400
+
+#FORBIDDEN ERROR
 @app.errorhandler(403)
 def forbidden(e):
     return render_template('403.html'), 403
 
-
+#NOT ALLOWED ERROR
 @app.errorhandler(405)
 def method_not_allowed(e):
     return render_template('405.html'), 405
 
-
+#NOT FOUND ERROR
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
-
+#SERVER INTERNAL ERROR
 @app.errorhandler(500)
 def server_error(e):
     return render_template('500.html'), 500
 
+#BAD GATEWAY
+@app.errorhandler(502)
+def server_error_bad_gateway(e):
+    return render_template('502.html'), 502
+
+#GATEWAY TIMEOUT
+@app.errorhandler(504)
+def server_error_gateway_timeout(e):
+    return render_template('504.html'), 504
+
+#LOGIN PAGE INDEX
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/boa/input", methods=['GET', 'POST'])
+#INPUT MACHINE AND PART NUMBER FORM
+@app.route("/boa/input1", methods=['GET', 'POST'])
 def boaInput():
-    if "step" not in request.form:
-        return render_template("boa-input.html", step="step-1")
-    elif request.form["step"] == "step-2":
-        return render_template("boa-input.html", step="step-2")
-    elif request.form["step"] == "step-3":
-        return render_template("boa-input.html", step="step-3")
+    if request.method == 'POST':
+        machines = request.form['machines']
+        parts = request.form['parts']
+        return render_template("boa-input-1.html", machines=machines, parts=parts)
+    return render_template("boa-input.html", machines=0, parts=0)
+
+#INPUT PARTS PRODUCED BY EACH MACHINE
+@app.route("/boa/input2", methods=['GET', 'POST'])
+def boaInput2():
+    if request.method == 'POST':
+        return render_template("boa-input-1.html")
+    return render_template("boa-input-1.html")
+
+#VIEW THE OUT CELLS FORMATION BY BOA  
+@app.route("/boa/input3", methods=['GET', 'POST'])
+def boaInput3():
+    if request.method == 'POST':
+        return render_template("boa-output.html")
+    return render_template("boa-output.html")
 
