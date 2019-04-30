b07eb41bc18f6119cee0a040d44a0c112f975ded
Prince Maposa <princekudzaimaposa94@gmail.com>
2019-05-01 00:54:34 +0200

    config file fix

--- config.py
+++ config.py
@@ -1,11 +1,28 @@
 import os
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-CSRF_ENABLED = True
 
-#SQLALCHEMY DATABASE CONFIGURATIONS
-if os.environ.get('DATABASE_URL') is None:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'ime.db')
-else:
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'theIme2019#'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
